6b1ef0151be7096301d0669cb7bdc1a8918542eb
LuanB-compt <luan-bruno-domingues@hotmail.com>
2023-02-22 14:03:50 -0300

    Finisehd the fifth class

--- aula5.py
+++ aula5.py
@@ -1,10 +1,22 @@
 """
-ARQUIVOS ESTÁTICOS
+MÉTODOS HTTP
 """
 
 import flask as fl
+import json
 
 app = fl.Flask(__name__, static_folder='static')
+
+@app.route("/")
+def index():
+    return fl.render_template(template_name_or_list="index5.html")
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if fl.request.method == "POST":
+        return json.dumps(fl.request.form)
+    elif fl.request.method == "GET":
+        return "get method"
 
 if __name__ == "__main__":
     app.run(debug=True)
